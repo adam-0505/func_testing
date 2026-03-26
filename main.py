05e3051ea2d30c649d2cea3d475fc65939c087d0
Adam <adamfischbein2005@gmail.com>
2026-03-26 20:33:08 +0100

    add functions: calculate_max, calculate_min, outliers, is_valid_value

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 def average(measurements):
+    if len(measurements) == 0:
+        return
+    
     sum = 0
+
     for value in measurements:
         sum += value
+    
     return sum / len(measurements)
+
+def calculate_max(measurements):
+    maximum = measurements[0]
+
+    for value in measurements:
+        if value > maximum:
+            maximum = value
+
+    return maximum
+
+def calculate_min(measurements):
+    minimum = measurements[0]
+
+    for value in measurements:
+        if value < minimum:
+            minimum = value
+
+    return minimum
+
+def outliers(measurements, threshold):
+    avg = average(measurements)
+    measurement_outliers = []
+
+    for value in measurements:
+        diff = avg - value
+        if diff > threshold or diff < -threshold:
+            measurement_outliers.append(value)
+
+    return measurement_outliers
+
+
+def is_valid_value(measurement):
+    return measurement >= 0 and measurement <= 40
